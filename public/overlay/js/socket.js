9cfcab348c00c2cb3534200204e78024a466bacb
Sydney Sisco <hello@sydsis.co>
2023-03-17 11:16:59 -0700

    draggable boxes with random emojis

--- public/overlay/js/socket.js
+++ public/overlay/js/socket.js
@@ -1,6 +1,9 @@
 const socket = io();
 
-window.onload = () => {};
+
+window.onload = () => {
+  draggable(document.getElementById('orb'));
+};
 
 const playAlarm = () => {
   // const audio = new Audio('media/alarm.mp3');
@@ -17,3 +20,84 @@ socket.on('play', (data) => {
   var audio = new Audio('media/' + data.sound);
   audio.play();
 });
+
+
+const newBox = () => {
+  // get text from input or default to a random emoji
+  const text = document.getElementById('label').value || randomEmoji();
+  // clear the input
+  document.getElementById('label').value = '';
+
+
+
+  const box = document.createElement('div');
+  box.classList.add('orb');
+  box.innerHTML = text;
+  box.style.left = 0;
+  box.style.top = 0;
+  document.body.appendChild(box);
+  draggable(box);
+};
+
+/**
+ * Makes an element draggable.
+ *
+ * @param {HTMLElement} element - The element.
+ */
+function draggable(element) {
+  var isMouseDown = false;
+
+  // initial mouse X and Y for `mousedown`
+  var mouseX;
+  var mouseY;
+
+  // element X and Y before and after move
+  var elementX = 0;
+  var elementY = 0;
+
+  // mouse button down over the element
+  element.addEventListener('mousedown', onMouseDown);
+
+  /**
+   * Listens to `mousedown` event.
+   *
+   * @param {Object} event - The event.
+   */
+  function onMouseDown(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    isMouseDown = true;
+  }
+
+  // mouse button released
+  element.addEventListener('mouseup', onMouseUp);
+
+  /**
+   * Listens to `mouseup` event.
+   *
+   * @param {Object} event - The event.
+   */
+  function onMouseUp(event) {
+    isMouseDown = false;
+    elementX = parseInt(element.style.left) || 0;
+    elementY = parseInt(element.style.top) || 0;
+  }
+
+  // need to attach to the entire document
+  // in order to take full width and height
+  // this ensures the element keeps up with the mouse
+  document.addEventListener('mousemove', onMouseMove);
+
+  /**
+   * Listens to `mousemove` event.
+   *
+   * @param {Object} event - The event.
+   */
+  function onMouseMove(event) {
+    if (!isMouseDown) return;
+    var deltaX = event.clientX - mouseX;
+    var deltaY = event.clientY - mouseY;
+    element.style.left = elementX + deltaX + 'px';
+    element.style.top = elementY + deltaY + 'px';
+  }
+}
