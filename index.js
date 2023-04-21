c27dd0c8fd99d1ec13727029e1108307412bdce5
Sydney Sisco <hello@sydsis.co>
2023-04-20 22:02:45 -0700

    give each box a random id

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const { Server } = require("socket.io")
 const io = new Server(server);
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+const { v4: uuidv4 } = require('uuid');
 
 // serve static files from public directory
 app.use(express.static('public'))
 
-// features
-// require('./twitch.js')(io);
+
 require('./twitch.js')((message) => {
   io.emit("twitch", message);
-  // io.emit("addBox", {
-  //   key: id,
-  //   timerDuration: '',
-  //   title: message
-  // });
+
+  const boxId = uuidv4();
+
+  io.emit("addBox", {
+    ...message,
+    // random between 140 and 500 
+    left: Math.floor(Math.random() * (500 - 40 + 1) + 40),
+    // random between 30 and 500
+    top: Math.floor(Math.random() * (500 - 30 + 1) + 30),
+    duration: '',
+    title: `${message.user}: ${message.message}`,
+  });
 });
 
 
